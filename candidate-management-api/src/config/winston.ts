941c8b149d575428a8a4d74846fed2e472752e00
joelsteven <joelofelectronics@gmail.com>
2024-06-26 14:37:15 +0300

    chore: Add winston logger configuration

---
+++ candidate-management-api/src/config/winston.ts
@@ -0,0 +1,39 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, printf, colorize } = format;
+
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(
+    colorize(),
+    timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    customFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "logs/error.log", level: "error" }),
+    new transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: combine(
+        colorize(),
+        timestamp({
+          format: "YYYY-MM-DD HH:mm:ss",
+        }),
+        customFormat
+      ),
+    })
+  );
+}
+
+export default logger;
