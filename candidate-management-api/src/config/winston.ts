ef40442f450746c00d8ba71105f451c5bf00abac
joelsteven <joelofelectronics@gmail.com>
2024-06-26 20:52:55 +0300

    chore: Remove console transport from winston configuration

--- candidate-management-api/src/config/winston.ts
+++ candidate-management-api/src/config/winston.ts
@@ -16,7 +16,6 @@ const logger = createLogger({
     customFormat
   ),
   transports: [
-    new transports.Console(),
     new transports.File({ filename: "logs/error.log", level: "error" }),
     new transports.File({ filename: "logs/combined.log" }),
   ],
