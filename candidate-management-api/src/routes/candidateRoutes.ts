8a29babe264490576ad6a4393d03c85d0464b6cd
joelsteven <joelofelectronics@gmail.com>
2024-06-26 20:55:27 +0300

    feat: Add candidate/s management routes for CRUD

--- candidate-management-api/src/routes/candidateRoutes.ts
+++ candidate-management-api/src/routes/candidateRoutes.ts
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import { candidateSchema } from "../validators/candidateValidator";
+import {
+  addOrUpdateCandidate,
+  getCandidateByEmail,
+  getAllCandidates,
+  deleteCandidate,
+} from "../controllers/candidateController";
 import validate from "../middleware/validate";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from candidate routes!");
-});
+router.post("/", validate(candidateSchema), addOrUpdateCandidate);
 
-router.post("/", validate(candidateSchema), (req, res) => {
-  res.send("Candidate created successfully!");
-});
+router.get("/:email", getCandidateByEmail);
+
+router.get("/", getAllCandidates);
+
+router.delete("/:email", deleteCandidate);
 
 export default router;
