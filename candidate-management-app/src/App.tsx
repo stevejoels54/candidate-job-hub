9d12a57c73fa69b5bdc89c9ea282655ad5d24a66
joelsteven <joelofelectronics@gmail.com>
2024-06-28 18:40:13 +0300

    refactor: Update CSS styles for candidate management app

--- candidate-management-app/src/App.tsx
+++ candidate-management-app/src/App.tsx
@@ -1,5 +1,6 @@
 import AppLayout from "./layout/AppLayout";
 import Candidates from "./pages/candidates/Candidates";
+import "./App.css";
 
 function App() {
   return <AppLayout children={<Candidates />} />;
