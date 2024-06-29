ac9be9e076beef008089672c0bf9b53000258c9f
joelsteven <joelofelectronics@gmail.com>
2024-06-30 00:21:39 +0300

    chore: Update welcome route for the API with available routes

--- candidate-management-api/src/app.ts
+++ candidate-management-api/src/app.ts
@@ -16,6 +16,21 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use("/api/candidates", candidateRoutes);
 
+// Welcome route for the API
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Welcome to the Candidate Management API</h1>
+    <p>Available routes:</p>
+    <ul>
+      <li><strong>GET /api/candidates</strong>: Retrieve a list of all candidates.</li>
+      <li><strong>POST /api/candidates</strong>: Add a new candidate. Expects a candidate object in the request body.</li>
+      <li><strong>GET /api/candidates/:email</strong>: Retrieve the details of a specific candidate by their Email.</li>
+      <li><strong>PUT /api/candidates/:email</strong>: Update the details of a specific candidate by their Email. Expects updated candidate data in the request body.</li>
+      <li><strong>DELETE /api/candidates/:email</strong>: Delete a specific candidate by their Email.</li>
+    </ul>`
+  );
+});
+
 // catch all other resourses
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Resource Not Found!" })
